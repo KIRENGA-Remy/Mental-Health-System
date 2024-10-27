dd4bfde1a17fa4d7c909982d7c6d3329caf0b1f3
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-10-27 00:04:11 -0700

    connection to database

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,7 @@
 # db_connection.py
 import pymongo
 
-url = 'mongodb://localhost:27017'
+url = 'mongodb://localhost:27017/mental_health_system'
 client = pymongo.MongoClient(url)
-db = client['mental_health_system']
+# db = client['mental_health_system']
+db=client.get_database()
--- mental_health_system/settings.py
+++ mental_health_system/settings.py
@@ -56,13 +56,14 @@ ROOT_URLCONF = 'mental_health_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Add the base templates directory here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
